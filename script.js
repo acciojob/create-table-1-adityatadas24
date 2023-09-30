912b293b044ecec21264992b08beb29524d83d61
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-30 14:06:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
+	const sampleRow = document.getElementById("sampleTable");
+
+	const tr = document.createElement("tr");
+	tr.innerHTML = "<td>New Cell1</td><td>New Cell2</td>";
+
+	sampleRow.appendChild(tr);
+	sampleRow.insertAfter(tr);
+	
+	
+	
   
   
 }
